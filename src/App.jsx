0b569fca7cebf3f32e150032bb923abd8e1c4a81
boikanyobaitshepi <boikanyobaitshepi@gmail.com>
2024-06-21 10:41:26 +0200

    imported layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
 import React from 'react';
-import {BrowserRouter ,Routes, Route, } from 'react-router-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import PodcastList from './pages/podcastlist';
 import PodcastDetail from './pages/podcastdetails';
 // import Header from './components/Header'
 import Layout from './components/Layout';
-
-import {Link, } from 'react-router-dom';
-import AudioPlayer from './components/AudioPlayer'
-
-
-
-
-
+// import Preview from './pages/preview'; // Uncomment if you have a Preview component
 
 function App() {
   return (
-  <BrowserRouter>
-    <Routes>
-        <Route path='/' element={<Layout/>} >
-        <Route index element={<PodcastList />} />
-        <Route path='podcast/:id' element={<PodcastDetail />} />
-        {/* <Route path='preview' element={<Preview />} /> */}
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Layout />} >
+          <Route index element={<PodcastList />} />
+          <Route path='podcast/:id' element={<PodcastDetail />} />
+          {/* <Route path='preview' element={<Preview />} /> */}
         </Route>
-       
-        
-    </Routes>
-  </BrowserRouter>
-       
- )
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
